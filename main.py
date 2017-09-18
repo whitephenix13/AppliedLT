a3f93762ad86f7403e5cdbde2289121f094324c6
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-09-18 12:12:26 +0200

    corrected first algorithm by doing a broader search, seems to be working

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ f_align = open(DATA_DIR + 'file.aligned', 'r')
 
 #phrase is the phrase as a string
 #alignment is a list of pairs as follow : [[0,0],[0,1],[1,2]] ...
-def extractPhrases(phrase,alignements):
+#The idea of the algorithm is to check if each word alone can be a phrase and to extend those phrase to get all possible combination
+def extractPhrases(phrase,alignments):
     words = phrase.split()
-    prevBox=[-1,-1,-1,-1] #Used to check if a word align outside of the phrase
-    currentPhrase = "" # The current small phrase that is being built
+    currentPhrase = [] # The current small phrases that are being built
+    currentBoxes = [] # The corresponding boxes to the current phrases
 
-    smallestPhrases=[] # A small phrase is defined by a phrase in which if you remove a word, it will no longer be a phrase
+    allPhrases=[] # A small phrase is defined by a phrase in which if you remove a word, it will no longer be a phrase
     for word_ind in range(len(words)) :
-        if (currentPhrase == ""):
-            currentPhrase = words[word_ind]
-        else:
-            currentPhrase += " " + words[word_ind]
+        wordAlignments = findWordAlignments(word_ind,alignments)
+        #Add a new box and a new phrase for the word aline
+        currentBoxes.append([-1,-1,-1,-1])
+        currentPhrase.append("")
+        for i in range(len(currentBoxes)):
+            if currentPhrase[i] == "":
+                currentPhrase[i] = words[word_ind]
+            else:
+                currentPhrase[i] += " " + words[word_ind]
 
-        if(checkCorrectPhrase(prevBox,word_ind,alignements)):
-            smallestPhrases.append(currentPhrase)
-            currentPhrase=""
-            prevBox = [-1, -1, -1, -1]
+            correct, newBox = checkCorrectPhrase(currentBoxes[i],word_ind,wordAlignments)
+            if correct :
+                allPhrases.append(currentPhrase[i])
+            currentBoxes[i]= newBox
 
-        else:
-            prevBox = computePhraseBox(prevBox, alignements[word_ind])
-
-    if(len(currentPhrase)>0):
-        smallestPhrases.append(currentPhrase)
-    print("Smallest phrases : " + str(smallestPhrases))
-    return generateAllPhrases(smallestPhrases)
+    return allPhrases
 #Check if a phrase is correct by looking if no words are aligned to the outside
 #newWordIndex: index of the new word in the phrase
 #alignement: list of pairs as follow : [[0,0],[0,1],[1,2]] ...
-def checkCorrectPhrase(prevBox,newWordIndex,alignements):
-    newBox= computePhraseBox(prevBox,alignements[newWordIndex])
+def checkCorrectPhrase(prevBox,newWordIndex,wordAlignments):
+    newBox= computePhraseBox(prevBox,wordAlignments)
     #test if there is no alignment of a word in the box (either en or de) to a word outside it
     #Maily, test if there is no alignement [x,y] such that x in the box but not y , or the opposite
     correct=True
@@ -45,41 +45,64 @@ def checkCorrectPhrase(prevBox,newWordIndex,alignements):
         if( (x_in_box and not y_in_box) or (not x_in_box and y_in_box)):
             correct=False
             break
-    return correct
+    return correct,newBox
+
+
+#Given a word index, compute all the alignments from this word to other words. If reverse parameter is true, check the opposite alignments
+#word_index: index of the word in the phrase
+#alignements: list of pairs as follow : [[0,0],[0,1],[1,2]] ...
+#reverse: if alignments is de -> en , set reverse to true to have the corresponding en -> de alignements
+def findWordAlignments(word_index,alignments,reverse=False):
+    allAlignments = []
+    for pair in alignments:
+        if( (not reverse and (word_index==pair[0])) or (reverse and (word_index==pair[1]) ) ):
+            allAlignments.append(pair)
+    return allAlignments
+
 # Compute the box associated to the phrase. The computation is done by extending the box due to the addition of a word to the phrase.
 #ie: box is [0,1,2,3] (alignments are within the points (0,1) and (2,3)) and we want to add a new word which has the following alignment:
 #(2,4), the box will then be extended as [0,1,2,4]
 # prevBox: index of the words in the phrase: This is my desk , "my desk" => [2,3],  [-1,-1,-1,-1] if none
 # newAlignment: alignement corresponding to the new word in the format [x,y]
 def computePhraseBox(prevBox, newAlignment):
-    x=newAlignment[0]
-    y=newAlignment[1]
-    if(prevBox[0]==-1):
-        return [x,y,x,y]
-    else:
-        if(prevBox[0]>x):
-            prevBox[0]=x
-        if (prevBox[1] > y):
-            prevBox[1] = y
-        if (prevBox[2] < x):
-            prevBox[2] = x
-        if (prevBox[3] < y):
-            prevBox[3] = y
-    return prevBox
+    box=prevBox
+    for pair in newAlignment:
+        x=pair[0]
+        y=pair[1]
+        if(box[0]==-1):
+            box= [x,y,x,y]
+        else:
+            if(box[0]>x):
+                box[0]=x
+            if (box[1] > y):
+                box[1] = y
+            if (box[2] < x):
+                box[2] = x
+            if (box[3] < y):
+                box[3] = y
+    return box
 
-#Given small phrases, generate all combination of those to make a bigger one
-#ie: smallestPhrases = ["I","dream of","sleeping"] =>
-#['I', 'I dream of', 'I dream of sleeping', 'dream of', 'dream of sleeping', 'sleeping']
-def generateAllPhrases(smallestPhrases):
-    allPhrases=[]
-    for i in range(len(smallestPhrases)):
-        for j in range(i,len(smallestPhrases)):
-            newPhrase =smallestPhrases[i]
-            for k in range(i+1,j+1):
-                newPhrase+=(" "+ smallestPhrases[k])
-            allPhrases.append(newPhrase)
-    return allPhrases
 
-phrase = 'De minister van buitenlandse zaken brengt een bezoek aan The Nederland'
-alignements = [[0,0],[1,1],[2,2],[3,3],[4,3],[5,4],[6,4],[7,4],[8,4],[9,5],[9,6]]
+phrase = 'De minister van buitenlandse zaken brengt een bezoek aan Nederland vandaag'
+alignements = [[0,0],[1,1],[2,2],[3,3],[4,3],[5,4],[6,4],[7,4],[8,4],[9,5],[9,6],[10,7]]
 print(extractPhrases(phrase,alignements))
+#Expect ['De', 'De minister', 'De minister van', 'De minister van buitenlandse zaken', 'De minister van buitenlandse zaken brengt een bezoek aan',
+#  'De minister van buitenlandse zaken brengt een bezoek aan Nederland',
+# 'De minister van buitenlandse zaken brengt een bezoek aan Nederland vandaag',
+# 'minister', 'minister van', 'minister van buitenlandse zaken', 'minister van buitenlandse zaken brengt een bezoek aan',
+# 'minister van buitenlandse zaken brengt een bezoek aan Nederland', 'minister van buitenlandse zaken brengt een bezoek aan Nederland vandaag',
+# 'van', 'van buitenlandse zaken', 'van buitenlandse zaken brengt een bezoek aan', 'van buitenlandse zaken brengt een bezoek aan Nederland',
+# 'van buitenlandse zaken brengt een bezoek aan Nederland vandaag', 'buitenlandse zaken', 'buitenlandse zaken brengt een bezoek aan',
+# 'buitenlandse zaken brengt een bezoek aan Nederland', 'buitenlandse zaken brengt een bezoek aan Nederland vandaag', 'brengt een bezoek aan',
+# 'brengt een bezoek aan Nederland', 'brengt een bezoek aan Nederland vandaag', 'Nederland', 'Nederland vandaag', 'vandaag']
+
+phrase = 'a b c' #mapped to a1 b1 c1 d1 as followed
+alignements = [[0,0],[1,1],[2,2],[0,3]]
+print(extractPhrases(phrase,alignements))
+
+#test with today(beginning) and vandaag(end)
+#Problem :      a1   b1   c1   a2
+#            a  .              .
+#            b       .
+#            c           .
+#Desired output : b, c, bc, abc
